f9ff89f1abdb6f893683d1b706ff18d2bd60aa31
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-11-07 16:06:12 +0000

    Added program

---
+++ factorials.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by Cristiano Sellitto
+# Created in October 2022
+# A program that finds the factorial of a number
+
+
+def main():
+    # Finds the factorial of a number
+
+    number = input("Enter a number to calculate the factorial: ")
+    try:
+        integer = int(number)
+        if integer > 0:
+            answer = 1
+            counter = 1
+            while counter < integer:
+                answer = answer * counter
+                counter = counter + 1
+            answer = integer * answer
+            print("\n{0}! = {1}".format(integer, answer))
+        else:
+            print("\nError: {} is a negative integer.".format(integer))
+    except ValueError:
+        print("\nError: {} is not an integer.".format(number))
+    finally:
+        print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
